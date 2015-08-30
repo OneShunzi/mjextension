cbafa7d00d3e8bce1b7cc4aa548b51723b564970
王顺子 <wangshunzi@wangshunzidembp>
2015-08-30 15:32:54 +0800

    初始化项目

---
+++ mjextion/mjextion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mjextion
+//
+//  Created by 王顺子 on 15/8/30.
+//  Copyright © 2015年 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
